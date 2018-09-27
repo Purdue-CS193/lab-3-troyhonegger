4b9d0bf52bec3d55b2c5ab78829bd5e3bdb54318
troyhonegger <troyhonegger@gmail.com>
2018-09-27 14:54:52 -0400

    added 1 comment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,6 +1,7 @@
 
 public class BuggyUtilClient {
     public static void main(String[] args) {
+        // Some changes
         // First test case in Main
         System.out.println("Example 1: Make My Username");
         System.out.println("In: first = " + "\"Sean\"" +
